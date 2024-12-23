dad48dd04cad9ee0204664e612cf9c0061878e56
HallowTommy <151835761+HallowTommy@users.noreply.github.com>
2024-12-23 16:55:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file, jsonify
 from TTS.api import TTS
 from pydub import AudioSegment
 import os
-import uuid
 
 app = Flask(__name__)
 
 # Инициализация Coqui TTS с простой моделью
 MODEL_NAME = "tts_models/en/ljspeech/tacotron2-DDC"
 tts = TTS(MODEL_NAME, progress_bar=False)
-
-STATIC_DIR = "static"
-os.makedirs(STATIC_DIR, exist_ok=True)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -27,44 +23,19 @@ def generate_audio():
         if not text:
             return jsonify({"error": "Text is required"}), 400
 
-        # Генерируем уникальное имя файла
-        output_filename = f"{uuid.uuid4().hex}.wav"
-        output_path = os.path.join(STATIC_DIR, output_filename)
-
         # Генерируем аудиофайл
+        output_path = "output.wav"
         tts.tts_to_file(text=text, file_path=output_path)
 
         # Преобразуем голос с фиксированным значением pitch_factor = 0.6
-        processed_filename = f"processed_{uuid.uuid4().hex}.wav"
-        processed_path = os.path.join(STATIC_DIR, processed_filename)
+        processed_path = "processed_output.wav"
         lower_pitch(output_path, processed_path)
 
-        # Удаляем исходный файл
-        os.remove(output_path)
-
-        # Формируем полный URL для обработанного файла
-        full_url = f"https://tacotrontts-production.up.railway.app/{STATIC_DIR}/{processed_filename}"
-
-        # Возвращаем JSON-ответ
-        response = jsonify({"audio_url": full_url})
-        response.headers["Content-Type"] = "application/json"  # Явно указываем JSON
-        return response
+        # Отправляем обработанный файл пользователю
+        return send_file(processed_path, as_attachment=True)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-@app.route("/delete", methods=["POST"])
-def delete_file():
-    try:
-        data = request.get_json()
-        file_path = data.get("file_path")
-
-        if file_path and os.path.exists(file_path):
-            os.remove(file_path)
-            return jsonify({"status": "success", "message": "File deleted successfully."})
-        return jsonify({"status": "error", "message": "File not found."})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
 
 def lower_pitch(input_path, output_path):
     """
