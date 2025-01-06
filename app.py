9365ecdd9b4fb2385d98830301738e21f5db2c5f
HallowTommy <151835761+HallowTommy@users.noreply.github.com>
2025-01-06 22:03:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ tts = TTS(MODEL_NAME, progress_bar=False)
 logger.info("TTS model initialized: %s", MODEL_NAME)
 
 # Настройки для VPS
-VPS_HOST = "95.179.247.70"  # IP-адрес вашего VPS
+VPS_HOST = "95.179.190.232"  # IP-адрес вашего VPS
 VPS_USERNAME = "root"       # Имя пользователя
 VPS_PASSWORD = "2c}Gd)QMri,@#f$Y"     # Пароль
 VPS_DEST_PATH = "/tmp/tts_files"  # Путь для хранения файлов на VPS
